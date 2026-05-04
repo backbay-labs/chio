2b7d17e5cf95a28173c32a766c7e80ca168ab0ed
ConnorWhelan11 <whelan_connor@yahoo.com>
2026-05-04 15:45:44 -0400

    fix(fuzz): map federation trust targets

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -39,6 +39,7 @@ TARGETS=(
     canonical_json
     capability_receipt
     manifest_roundtrip
+    federation_trust_establishment
     fuzz_policy_parse_compile
     fuzz_sql_parser
     fuzz_merkle_checkpoint
--- infra/oss-fuzz/build.sh
+++ infra/oss-fuzz/build.sh
@@ -33,6 +33,7 @@ TARGETS=(
     canonical_json
     capability_receipt
     manifest_roundtrip
+    federation_trust_establishment
     fuzz_policy_parse_compile
     fuzz_sql_parser
     fuzz_merkle_checkpoint
